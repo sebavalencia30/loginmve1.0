f8e789bb1df24ab3515fba58b901e8899ce32f99
sebavalencia30 <64997738+sebavalencia30@users.noreply.github.com>
2020-12-02 04:48:25 -0300

    Update register.page.ts

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { PostProvider } from '../../providers/post-provider';
-import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -10,64 +7,9 @@ import { ToastController } from '@ionic/angular';
 })
 export class RegisterPage implements OnInit {
 
-	rut: string = "";
-	nombre: string = "";
-	apellido: string = "";
-	direccion: string = "";
-	celular: string = "";
-	email: string = "";
-	password: string = "";
-	password2: string = "";
-
-
-  constructor(
-  	private router: Router,
-  	private PostPvdr : PostProvider,
-  	public toastCtrl: ToastController
-  ) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  formLogin(){
- 	this.router.navigate(['/login']);
- 	}
-
-  async Register(){
-  		
-  		if(this.email==""){
-        const toast = await this.toastCtrl.create({
-          message: 'Email es requerido',
-          duration: 3000
-        });
-        toast.present();
-    }else if(this.password==""){
-        const toast = await this.toastCtrl.create({
-          message: 'Password es requerido',
-          duration: 3000
-        });
-        toast.present();
-    }else if(this.password!=this.password2){
-        const toast = await this.toastCtrl.create({
-          message: 'Las contraseñas no coinciden',
-          duration: 3000
-        });
-        toast.present();}
-
-        else{
-
-		  	let body = {
-	  		rut: this.rut,
-	  		nombre: this.nombre,
-	  		apellido: this.apellido,
-	  		direccion: this.direccion,
-	  		celular: this.celular,
-	  		email: this.email,
-	  		password: this.password,
-	  		aksi: 'register'
-	  		};
-
-  		
-      }
-      }
-      }
+}
