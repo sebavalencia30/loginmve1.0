8ac8efc45e98de902b91b1ccc72d4db117c12c53
sebavalencia30 <64997738+sebavalencia30@users.noreply.github.com>
2020-12-02 04:25:04 -0300

    Update register.page.ts

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -67,7 +67,7 @@ export class RegisterPage implements OnInit {
 	  		aksi: 'register'
 	  		};
 
-  		this.PostPvdr.postData(body, 'proses-api.php').Subscriber(async data =>{
+  		this.PostPvdr.postData(body, 'proses-api.php').unsubscribe(async data =>{
         var alertmsg = data.msg;
         if(data.success){
           
