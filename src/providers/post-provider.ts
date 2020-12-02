f28096380f390858ef658c9a9e224d569e76069a
sebavalencia30 <64997738+sebavalencia30@users.noreply.github.com>
2020-12-02 01:48:55 -0300

    Update post-provider.ts

--- src/providers/post-provider.ts
+++ src/providers/post-provider.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
+import { HttpClient, HttpHeaders , RequestOptions } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class PostProvider {
 	server: string = "http://localhost/server_api/"; 
 
-	constructor(public http : Http) {
+	constructor(public http : HttpClient) {
 
 	}
 
 	postData(body, file){
 		let type = "application/json; charset=UTF-8";
-		let headers = new Headers({ 'Content-Type': type });
+		let headers = new HttpHeaders({ 'Content-Type': type });
 		let options = new RequestOptions({ headers: headers });
 
 		return this.http.post(this.server + file, JSON.stringify(body), options)
